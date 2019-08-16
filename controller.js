fc5c62dd7bc20a9b981a9c3c0a53632953d19b96
fastaman993 <fastabiqul.khoirot993@gmail.com>
2019-08-17 05:55:23 +0700

    create

--- controller.js
+++ controller.js
@@ -1,6 +1,38 @@
 const db = require('./connect');
 
 module.exports = {
+
+  create: (req, res) => {
+    let { name, brand, type, branch, price } = req.body
+    
+    db.query(`INSERT INTO tbToko SET name=?, brand=?, type=?, branch=?, price=?`, 
+      [name, brand, type, branch, price], 
+      function(error, response) {
+        if(error) {
+          console.log(error);
+          
+        } else {
+          let formResponse = {
+            status: 201,
+            data: response
+          }
+          res.json(formResponse)
+        }
+      })
+  },
+
+  update: (req, res) => {
+    let id = req.params.id
+    let { name, brand, type, branch, price } = req.body
+
+    db.query(`UPDATE tbToko SET name=?, brand=?, type=?, branch=?, price=?`,
+    [name, brand.])
+  },
+
+  remove: (req, res) => {
+
+  },
+
   findAll : (req,res) => {
     db.query('SELECT * FROM tbToko', function(error, response) {
       if(error) {
@@ -13,18 +45,6 @@ module.exports = {
   },
 
   findBy : (req, res) => {
-
-  },
-
-  create: (req, res) => {
-
-  },
-
-  update: (req, res) => {
-
-  },
-
-  remove: (req, res) => {
-
+    db.query('')
   }
 }
--- routes.js
+++ routes.js
@@ -2,5 +2,6 @@ const routes = require('express').Router()
 const { findAll, findBy, create, update, remove } = require('./controller')
 
 routes.get('/', findAll)
+routes.post('/', create)
 
 module.exports = routes
