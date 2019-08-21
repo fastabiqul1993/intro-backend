73f59985a980a33a062a1b85c333a902a37ddace
fastaman993 <fastabiqul.khoirot993@gmail.com>
2019-08-21 19:25:02 +0700

    change DB

--- src/Configs/connect.js
+++ src/Configs/connect.js
@@ -2,10 +2,10 @@ require("dotenv/config");
 const mysql = require("mysql");
 
 const dbConfig = mysql.createConnection({
-  host: "db4free.net",
-  user: "fastaman",
-  password: "fastaman1993",
-  database: "dbheroku"
+  host: "w1kr9ijlozl9l79i.chr7pe7iynqr.eu-west-1.rds.amazonaws.com",
+  user: "pvq4inhhpgnusdwa",
+  password: "ii75nalmo8t8hscw",
+  database: "flf0ghak9lyhlqox"
   // socketPath: process.env.DB_SOCKET
 });
 
